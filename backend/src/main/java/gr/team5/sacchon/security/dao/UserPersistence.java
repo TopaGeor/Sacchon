2c3a42f78c661fd397ec942c6df28dc44a0f9671
RealOrge <george.top@windowslive.com>
2020-09-19 00:36:24 +0300

    Add comments

--- backend/src/main/java/gr/team5/sacchon/security/dao/UserPersistence.java
+++ backend/src/main/java/gr/team5/sacchon/security/dao/UserPersistence.java
@@ -3,18 +3,50 @@ package gr.team5.sacchon.security.dao;
 
 import org.restlet.Context;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Objects;
 
 /**
  * A class to configure the database
+ * And to set up the user
  * @author One-To-Fix-Them-All
  */
 public class UserPersistence {
     private static UserPersistence userPersistence = new UserPersistence();
-    private UserPersistence() {};
+    private UserPersistence() {}
+
+    /**
+     * Finds a user by his username
+     * @param username the user name of the user to find
+     * @return the user
+     * @throws SQLException if no user with username as name
+     */
+    public ApplicationUser findByUsername(String username) throws SQLException {
+        Context.getCurrentLogger().finer(
+                "Method findById of UserPersistence started"
+        );
+
+        Connection connection = null;
+        try {
+            connection = getConnection();
+            PreparedStatement preparedStatement = connection
+                    .prepareStatement("select * from UserTable where username=?");
+            preparedStatement.setString(1, username);
+
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next()) {
+                ApplicationUser user = new ApplicationUser();
+                user.setUsername(rs.getString("username"));
+                user.setUsername(rs.getString("password"));
+                user.setUsername(rs.getString("role"));
+                return user;
+            }
+            return null;
+        } finally {
+            releaseConnection(connection);
+            Context.getCurrentLogger().finer("Method findById of UserPersistence ended");
+        }
+    }
 
     /** synchronized, is for multithreading only one thread has access to it **/
     public static synchronized UserPersistence getUserPersistence(){
