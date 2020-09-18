759a0749830c8587f91a1819075ce2ec9e86aa98
John Balatsos <westcoast1910@hotmail.com>
2020-09-18 14:09:27 +0300

    Correct routing for class Patient

--- backend/src/main/java/gr/team5/sacchon/router/CustomRouter.java
+++ backend/src/main/java/gr/team5/sacchon/router/CustomRouter.java
@@ -1,11 +1,11 @@
 package gr.team5.sacchon.router;
 
+import gr.team5.sacchon.resource.ConsultationResourceImpl;
 import gr.team5.sacchon.resource.PatientDataResourceImpl;
 import gr.team5.sacchon.resource.PatientResourceImpl;
 import gr.team5.sacchon.resource.PingServerResource;
 import org.restlet.Application;
 import org.restlet.routing.Router;
-import org.restlet.routing.TemplateRoute;
 
 public class CustomRouter {
 
@@ -25,9 +25,12 @@ public class CustomRouter {
 
         Router router = new Router(app.getContext());
 
-        router.attach("/patient", PatientDataResourceImpl.class);
-        router.attach("/patient/", PatientDataResourceImpl.class);
-        router.attach("/patient/{id}", PatientResourceImpl.class);
+//        router.attach("/patient", PatientResourceImpl.class);
+//        router.attach("/patient/", PatientDataResourceImpl.class);
+//        router.attach("/patient/{id}", PatientResourceImpl.class);
+//        router.attach("/patient/{id}/data", PatientDataResourceImpl.class);
+//        router.attach("/patient/{id}/consultation", ConsultationResourceImpl.class);
+//        router.attach("/patient/{id}/data/average", PatientDataResourceImpl.class);
 
         return router;
     }
