ad830464b4ee896b1bb8d089b6c2fda3b0fb5fa2
John Balatsos <westcoast1910@hotmail.com>
2020-09-19 22:21:56 +0300

    now the methods of these classes are able to get, update, post and delete. Also implement validate() methods in class ResourceValidator.

--- backend/src/main/java/gr/team5/sacchon/resource/ConsultationResourceImpl.java
+++ backend/src/main/java/gr/team5/sacchon/resource/ConsultationResourceImpl.java
@@ -1,37 +1,95 @@
 package gr.team5.sacchon.resource;
 
 import gr.team5.sacchon.exception.NotFoundException;
+import gr.team5.sacchon.model.Consultation;
+import gr.team5.sacchon.model.Doctor;
 import gr.team5.sacchon.repository.ConsultationRepository;
+import gr.team5.sacchon.repository.DoctorRepository;
 import gr.team5.sacchon.repository.util.JpaUtil;
 import gr.team5.sacchon.representation.ConsultationRepresentation;
+import gr.team5.sacchon.representation.DoctorRepresentation;
+import gr.team5.sacchon.security.ResourceUtils;
+import gr.team5.sacchon.security.Shield;
 import org.restlet.engine.Engine;
+import org.restlet.resource.ResourceException;
 import org.restlet.resource.ServerResource;
 
+import javax.persistence.EntityManager;
+import java.util.Optional;
 import java.util.logging.Logger;
 
 public class ConsultationResourceImpl extends ServerResource implements ConsultationResource {
 
     public static final Logger LOGGER = Engine.getLogger(ConsultationResourceImpl.class);
+
     private long id;
     private ConsultationRepository consultationRepository;
+    private EntityManager entityManager;
 
+    /**
+     * This release method closes the entityManager
+     */
+    @Override
+    protected void doRelease() {
+        entityManager.close();
+    }
+
+    /**
+     * Initializes the consultation repository
+     */
     @Override
     protected void doInit() {
 
         LOGGER.info("Initializing consultation resource starts");
 
         try {
-            consultationRepository = new ConsultationRepository(JpaUtil.getEntityManager());
+            entityManager = JpaUtil.getEntityManager();
+            consultationRepository = new ConsultationRepository(entityManager);
             id = Long.parseLong(getAttribute("id"));
         } catch (Exception e) {
-            id = -1;
+            throw  new ResourceException(e);
         }
 
         LOGGER.info("Initializing patient resource ends");
     }
 
+    /**
+     *
+     * @return consultation by id
+     * @throws NotFoundException
+     */
     @Override
     public ConsultationRepresentation getConsultation() throws NotFoundException {
-        return null;
+
+        LOGGER.info("Retrieve a consultation");
+
+        // Checking authorization
+        ResourceUtils.checkRole(this, Shield.ROLE_DOCTOR);
+
+        // Initialize persistence layer
+        ConsultationRepository consultationRepository = new ConsultationRepository(entityManager);
+        Consultation consultation;
+
+        try {
+            Optional<Consultation> oConsultation = consultationRepository.findById(id);
+
+            setExisting(oConsultation.isPresent());
+            if (!isExisting()) {
+
+                LOGGER.config("consultation id does not exist: " + id);
+                throw new NotFoundException("No consultation with id: " + id);
+            } else {
+
+                consultation = oConsultation.get();
+                LOGGER.finer("User allowed to retrieve a consultation.");
+                ConsultationRepresentation result = new ConsultationRepresentation(consultation);
+
+                LOGGER.finer("Consultation successfully retrieved.");
+
+                return result;
+            }
+        } catch (Exception e) {
+            throw new ResourceException(e);
+        }
     }
 }
--- backend/src/main/java/gr/team5/sacchon/resource/PatientListResourceImpl.java
+++ backend/src/main/java/gr/team5/sacchon/resource/PatientListResourceImpl.java
@@ -2,30 +2,117 @@ package gr.team5.sacchon.resource;
 
 import gr.team5.sacchon.exception.BadEntityException;
 import gr.team5.sacchon.exception.NotFoundException;
+import gr.team5.sacchon.model.Patient;
 import gr.team5.sacchon.repository.PatientRepository;
+import gr.team5.sacchon.repository.util.JpaUtil;
 import gr.team5.sacchon.representation.PatientRepresentation;
+import gr.team5.sacchon.security.ResourceUtils;
+import gr.team5.sacchon.security.Shield;
 import org.restlet.engine.Engine;
+import org.restlet.resource.ResourceException;
 import org.restlet.resource.ServerResource;
 
+import javax.persistence.EntityManager;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class PatientListResourceImpl  extends ServerResource implements PatientListResource {
+public class PatientListResourceImpl extends ServerResource implements PatientListResource {
 
     public static  final Logger LOGGER = Engine.getLogger(PatientResourceImpl.class);
+
     private PatientRepository patientRepository;
+    private EntityManager entityManager;
 
-    protected void doInit() {
-
+    /**
+     * This release method closes the entityManager
+     */
+    @Override
+    protected void doRelease() {
+        entityManager.close();
     }
 
+    /**
+     * Initializes the patient repository
+     */
+    protected void doInit() {
+
+        LOGGER.info("Initializing patient list resource starts");
+
+        try {
+            entityManager = JpaUtil.getEntityManager();
+            patientRepository = new PatientRepository(entityManager);
+        } catch (Exception e) {
+            throw new ResourceException(e);
+        }
+
+        LOGGER.info("Initializing patient list resource ends");
+    }
+
+    /**
+     *
+     * @param patientReprIn representation of a Patient given by the frontEnd
+     * @return a representation of the persisted object
+     * @throws BadEntityException
+     */
     @Override
     public PatientRepresentation add(PatientRepresentation patientReprIn) throws BadEntityException {
-        return null;
+
+        LOGGER.finer("Add new patient.");
+
+        // Check authorization
+        ResourceUtils.checkRole(this, Shield.ROLE_PATIENT);
+        LOGGER.finer("User allowed to add a patient.");
+
+        // Check entity
+
+        if (patientReprIn == null) throw new BadEntityException("Bad entity");
+
+        LOGGER.finer("patient checked");
+
+        try {
+
+            // Convert PatientRepresentation to Patient
+            Patient patient = patientReprIn.createPatient();
+
+            Optional<Patient> patientOptOut = patientRepository.save(patient);
+
+            Patient patientOut;
+            if (patientOptOut.isPresent())
+                patientOut = patientOptOut.get();
+            else
+                throw new BadEntityException("Patient has not been created");
+
+            PatientRepresentation result = new PatientRepresentation(patientOut);
+
+            LOGGER.finer("Patient successfully added.");
+
+            return result;
+        } catch (Exception ex) {
+            LOGGER.log(Level.WARNING, "Error when adding a patient", ex);
+            throw new ResourceException(ex);
+        }
     }
 
     @Override
     public List<PatientRepresentation> getPatients() throws NotFoundException {
-        return null;
+
+        LOGGER.finer("Select all patients.");
+        // Check authorization
+        ResourceUtils.checkRole(this, Shield.ROLE_DOCTOR);
+
+        try {
+
+            List<Patient> patients = patientRepository.findAll();
+            List<PatientRepresentation> result = new ArrayList<>();
+
+            patients.forEach(patient -> result.add(new PatientRepresentation(patient)));
+
+            return result;
+        } catch (Exception e) {
+            throw new NotFoundException("patients not found");
+        }
     }
 }
