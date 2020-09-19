0497fc2169221ca63137b2fd6d395737a909b042
John Balatsos <westcoast1910@hotmail.com>
2020-09-19 16:38:41 +0300

    interface PatientData implementation

--- backend/src/main/java/gr/team5/sacchon/resource/ConsultationResource.java
+++ backend/src/main/java/gr/team5/sacchon/resource/ConsultationResource.java
@@ -1,4 +1,7 @@
 package gr.team5.sacchon.resource;
 
+import org.restlet.resource.Get;
+
 public interface ConsultationResource {
+
 }
--- backend/src/main/java/gr/team5/sacchon/resource/PatientDataResourceImpl.java
+++ backend/src/main/java/gr/team5/sacchon/resource/PatientDataResourceImpl.java
@@ -1,6 +1,28 @@
 package gr.team5.sacchon.resource;
 
+import gr.team5.sacchon.exception.BadEntityException;
+import gr.team5.sacchon.exception.NotFoundException;
+import gr.team5.sacchon.representation.PatientDataRepresentation;
 import org.restlet.resource.ServerResource;
 
-public class PatientDataResourceImpl extends ServerResource {
+public class PatientDataResourceImpl extends ServerResource implements PatientDataResource {
+    @Override
+    public PatientDataRepresentation getPatientData() throws NotFoundException {
+        return null;
+    }
+
+    @Override
+    public void remove() throws NotFoundException {
+
+    }
+
+    @Override
+    public PatientDataRepresentation store(PatientDataRepresentation patientDataRepresentation) throws NotFoundException, BadEntityException {
+        return null;
+    }
+
+    @Override
+    public PatientDataRepresentation add(PatientDataRepresentation patientDataIn) throws BadEntityException {
+        return null;
+    }
 }
