0456ecd66e978b79b6da02596bac7b5f86a5ae3e
John Balatsos <westcoast1910@hotmail.com>
2020-09-26 12:15:17 +0300

    import template into sacchon project

--- frontend/sacchon/src/app/app.module.ts
+++ frontend/sacchon/src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+
 import { AppRoutingModule } from './app-routing.module';
+import { ComponentsModule } from "./components/components.module";
 import { AppComponent } from './app.component';
-import { AuthPageComponent } from './auth/auth-page/auth-page.component';
+import { AuthPageComponent } from './layouts/auth-page/auth-page.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ToastrModule } from 'ngx-toastr';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AdminLayoutComponent,
     AuthPageComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    NgbModule,
+    RouterModule,
     AppRoutingModule,
-    HttpClientModule
+    ComponentsModule,
+    HttpClientModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
