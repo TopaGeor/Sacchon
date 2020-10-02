02781fd1d04093aebfa4f5ba88c352868cbebf3d
aristotelissym <arisymeon97@gmail.com>
2020-10-02 16:17:22 +0300

    final changes

--- frontend/sacchon/src/app/chief-doctor/chief-doctor.service.ts
+++ frontend/sacchon/src/app/chief-doctor/chief-doctor.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Consultations } from '../doctor/consultations/consultations';
+import { Doctor } from '../shared/doctor';
 import { Patient } from '../shared/patient';
 import { PatientData } from '../shared/patient-data';
 import { NeedConsultComponent } from './need-consult/need-consult.component';
@@ -43,6 +44,17 @@ export class ChiefDoctorService {
   nonActivePatients(opts): Observable<Patient[]> {
     let url = this.app + "chief/noActivity";
     return this.http.get<Patient[]>(
+      url,
+      {
+        params: opts,
+        headers:new HttpHeaders({'Authorization': 'Basic ' + btoa( this.username+ ':' +this.password)})
+      }
+    )
+  }
+
+  nonActiveDoctor(opts): Observable<Doctor[]> {
+    let url = this.app + "chief/noActivity";
+    return this.http.get<Doctor[]>(
       url,
       {
         params: opts,
