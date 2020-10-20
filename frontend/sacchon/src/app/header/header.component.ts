9b2d45fb417ee44c33ec49c493cd1ab5c6187e55
RealOrge <george.top@windowslive.com>
2020-10-20 15:42:04 +0300

    better html format and supporing function routing

--- frontend/sacchon/src/app/header/header.component.ts
+++ frontend/sacchon/src/app/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, Route } from '@angular/router';
 import { AuthLayoutService } from '../login-layout/auth-layout.service';
 
 @Component({
@@ -47,6 +47,8 @@ export class HeaderComponent implements OnInit {
         this.service.roleHeader(credentials.role);
       }
     }
+
+    this.printpath('', this.router.config);
   }
 
   logout() {
@@ -54,5 +56,16 @@ export class HeaderComponent implements OnInit {
     this.isLogged = false;
     this.role = "";
     this.router.navigate(['../']);
+  }
+
+  printpath(parent: String, config: Route[]) {
+    for (let i = 0; i < config.length; i++) {
+      const route = config[i];
+      console.log(parent + '/' + route.path);
+      if (route.children) {
+        const currentPath = route.path ? parent + '/' + route.path : parent;
+        this.printpath(currentPath, route.children);
+      }
+    }
   }
 }
