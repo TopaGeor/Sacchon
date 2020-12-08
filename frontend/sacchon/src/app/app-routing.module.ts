19d05fdc4ab623855e271e2f5155cf7e578e7adb
RealOrge <george.top@windowslive.com>
2020-12-08 12:45:27 +0200

    patient now has is own personal routing

--- frontend/sacchon/src/app/app-routing.module.ts
+++ frontend/sacchon/src/app/app-routing.module.ts
@@ -2,20 +2,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { PatientDataDetailComponent } from './patient/patient-data-detail/patient-data-detail.component';
-import { PatientNullComponent } from './patient/patient-null/patient-null.component';
-import { PatientComponent } from './patient/patient/patient.component';
-import { PatientDataComponent } from './patient/patient-data/patient-data.component';
-import { PostPatientDataComponent } from './patient/post-patient-data/post-patient-data.component';
-import { PatientDetailsComponent } from './patient/patient-details/patient-details.component';
-import { PutPatientDataComponent } from './patient/put-patient-data/put-patient-data.component';
-import { DeletePatientDataComponent } from './patient/delete-patient-data/delete-patient-data.component';
-import { AverageComponent } from './patient/average/average.component';
-import { DeletePatientComponent } from './patient/delete-patient/delete-patient.component';
 import { LandpageComponent } from './login-layout/landpage/landpage.component';
 import { LoginComponent } from './login-layout/login/login.component';
-import { SetPatientToDoctorComponent } from './patient/set-patient-to-doctor/set-patient-to-doctor.component';
-import { PatientConsultationComponent } from './patient/patient-consultation/patient-consultation.component';
 
 
 const routes: Routes = [
@@ -29,57 +17,9 @@ const routes: Routes = [
   },
   {
     path: 'patient',
-    children: [
-      {
-        path: '',
-        component: PatientComponent,
-      },
-      {
-        path: ':id',
-        component: PatientDetailsComponent,
-      },
-      {
-        path: ':patientId/:doctorId/ok',
-        component: SetPatientToDoctorComponent,
-      },
-      {
-        path: ':id/data',
-        component: PatientDataComponent,
-
-      },
-      {
-        path: ':id/data/post',
-        component: PostPatientDataComponent,
-      },
-      {
-        path: ':patientId/data/average',
-        component: AverageComponent,
-      },
-      {
-        path: ':patientId/data/:dataId',
-        component: PatientDataDetailComponent,
-      },
-      {
-        path: ':patientId/data/:dataId/edit',
-        component: PutPatientDataComponent,
-      },
-      {
-        path: ':patientId/data/:dataId/delete',
-        component: DeletePatientDataComponent,
-      },
-      {
-        path: ':id/delete',
-        component: DeletePatientComponent,
-      },
-      {
-        path: ':id/consultations',
-        component: PatientConsultationComponent
-      },
-      {
-        path: ':patientId/:doctorId',
-        component: PatientNullComponent,
-      },
-    ]
+    loadChildren: () => 
+    import('./patient/patient.module')
+    .then(m => m.PatientModule)
   },
   {
     path: 'doctor',
--- frontend/sacchon/src/app/patient/patient-routing.module.ts
+++ frontend/sacchon/src/app/patient/patient-routing.module.ts
@@ -1,7 +1,68 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { PatientComponent } from '../patient/patient/patient.component';
+import { AverageComponent } from '../patient/average/average.component';
+import { PatientNullComponent } from '../patient/patient-null/patient-null.component';
+import { PatientDataComponent } from '../patient/patient-data/patient-data.component';
+import { DeletePatientComponent } from '../patient/delete-patient/delete-patient.component';
+import { PatientDetailsComponent } from '../patient/patient-details/patient-details.component';
+import { PutPatientDataComponent } from '../patient/put-patient-data/put-patient-data.component';
+import { PostPatientDataComponent } from '../patient/post-patient-data/post-patient-data.component';
+import { DeletePatientDataComponent } from '../patient/delete-patient-data/delete-patient-data.component';
+import { PatientDataDetailComponent } from '../patient/patient-data-detail/patient-data-detail.component';
+import { PatientConsultationComponent } from '../patient/patient-consultation/patient-consultation.component';
+import { SetPatientToDoctorComponent } from '../patient/set-patient-to-doctor/set-patient-to-doctor.component';
+
+const routes: Routes = [{
+  path: '',
+  component: PatientComponent,
+},
+{
+  path: ':id',
+  component: PatientDetailsComponent,
+},
+{
+  path: ':patientId/:doctorId/ok',
+  component: SetPatientToDoctorComponent,
+},
+{
+  path: ':id/data',
+  component: PatientDataComponent,
+
+},
+{
+  path: ':id/data/post',
+  component: PostPatientDataComponent,
+},
+{
+  path: ':patientId/data/average',
+  component: AverageComponent,
+},
+{
+  path: ':patientId/data/:dataId',
+  component: PatientDataDetailComponent,
+},
+{
+  path: ':patientId/data/:dataId/edit',
+  component: PutPatientDataComponent,
+},
+{
+  path: ':patientId/data/:dataId/delete',
+  component: DeletePatientDataComponent,
+},
+{
+  path: ':id/delete',
+  component: DeletePatientComponent,
+},
+{
+  path: ':id/consultations',
+  component: PatientConsultationComponent
+},
+{
+  path: ':patientId/:doctorId',
+  component: PatientNullComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
